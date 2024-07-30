e54b1d8abfa3532c107f5eed43715c34e0682f4e
artie-owlet <artie-owlet@protonmail.com>
2024-07-30 10:39:11 +0300

    feat: add PhotoId

--- src/schema/data/base-person-data.ts
+++ src/schema/data/base-person-data.ts
@@ -1,11 +1,16 @@
 import ss from 'superstruct';
 
-import type { FlavoredString } from '../../utils/flavored-string';
+import { defineFlavoredStringSchema, type FlavoredString } from '../../utils/flavored-string';
+
+const photoIdSchema = defineFlavoredStringSchema('PhotoId');
+
+export type PhotoId = ss.Infer<typeof photoIdSchema>;
 
 export function defineBasePersonDataSchema<T extends string>(id: ss.Struct<FlavoredString<T>, null>) {
     return ss.object({
         id,
         name: ss.string(),
+        photoId: photoIdSchema,
     });
 }
 
