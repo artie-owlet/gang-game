19722432c656b468b76ddbbf2465fd75675d537d
artie-owlet <artie-owlet@protonmail.com>
2024-08-05 11:23:04 +0300

    chore: wip

--- src/components/building.ts
+++ src/components/building.ts
@@ -1,12 +1,6 @@
 import ss from 'superstruct';
 
-import { createBaseClass } from '../utils/create-base-class';
-
 export const buildingSchema = ss.object({
     position: ss.number(),
     buildingName: ss.string(),
 });
-
-type BuildingData = ss.Infer<typeof buildingSchema>;
-
-export abstract class Building extends createBaseClass<BuildingData>() {}
--- src/components/manageable.ts
+++ src/components/manageable.ts
@@ -2,7 +2,6 @@ import ss from 'superstruct';
 
 import type { GameContext } from '../game-objects/game-context';
 import type { Gangster } from '../game-objects/gangster';
-import { createBaseClass } from '../utils/create-base-class';
 
 export const manageableSchema = ss.object({
     managerId: ss.nullable(ss.string()),
@@ -10,15 +9,40 @@ export const manageableSchema = ss.object({
 
 type ManageableData = ss.Infer<typeof manageableSchema>;
 
-export abstract class Manageable extends createBaseClass<ManageableData>() {
-    public constructor(
-        data: ManageableData,
-        private game: GameContext,
-    ) {
-        super(data);
+// export abstract class Manageable extends createBaseClass<ManageableData>() {
+//     public constructor(
+//         data: ManageableData,
+//         private game: GameContext,
+//     ) {
+//         super(data);
+//     }
+
+//     public get manager(): Gangster | null {
+//         return this.managerId ? this.game.gangster(this.managerId) : null;
+//     }
+// }
+
+export function Manageable<
+    Data extends object,
+    Ctx extends object,
+>(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    Base: new(data: Data, ctx: () => Ctx) => any,
+) {
+    interface Component extends ManageableData {}
+
+    abstract class Component extends Base {
+        public constructor(
+            data: ManageableData & Data,
+            protected ctx: () => Ctx & Pick<GameContext, 'gangster'>,
+        ) {
+            super(data, ctx);
+        }
+
+        public get manager(): Gangster | null {
+            return this.managerId ? this.ctx().gangster(this.managerId) : null;
+        }
     }
 
-    public get manager(): Gangster | null {
-        return this.managerId ? this.game.gangster(this.managerId) : null;
-    }
+    return Component;
 }
